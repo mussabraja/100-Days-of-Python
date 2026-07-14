86a7c3b37acb9e173adda2337ccb2f2feb46eaba
mussabraja <mussabraja@gmail.com>
2026-07-14 15:29:52 +0500

    Day 6 Reeborg maze solution

---
+++ day-06-reeborg/maze.py
@@ -0,0 +1,16 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        while wall_in_front():
+            turn_left()
+        move()
