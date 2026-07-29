defc51efb4df3fb4893f76202fb896e593f8eba7
Mussab Sabir <mussabraja@gmail.com>
2026-07-30 02:55:40 +0500

    Add Food class for snake game

---
+++ day-21-snake-game-complete/food.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.color('white')
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.goto(random.randint(-230, 230), random.randint(-230, 230))
+
+    def refresh(self):
+        random_x = random.randint(-230, 230)
+        random_y = random.randint(-230, 230)
+        self.goto(random_x, random_y)
