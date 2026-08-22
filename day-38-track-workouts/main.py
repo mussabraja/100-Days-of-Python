1925e9af5b6c09aadd99b8200b303acb557d1f95
Mussab Sabir <mussabraja@gmail.com>
2026-08-22 23:52:38 +0500

    Refactor workout tracking with Sheety integration
    Updated app_id and app_key placeholders, added datetime for logging, and structured request body for Sheety API.

--- day-38-track-workouts/main.py
+++ day-38-track-workouts/main.py
@@ -1,7 +1,8 @@
 import requests
+from datetime import datetime
 
-app_id = 'app_acd7ed1e6c9a46729df8fb92'
-app_key = 'nix_live_Wx1RT7ifyPzE5Cw3fkRUTE5T1bOeVqvW'
+app_id = 'your app id'
+app_key = 'your app key'
 
 
 url = 'https://app.100daysofpython.dev'
@@ -18,8 +19,33 @@ user_params =   {
   "query": "ran 3 miles",
   "weight_kg": 70,
 }
-
-
-
 response = requests.post(url = url_post, json=user_params, headers=header)
-print(response.json())
+data = response.json()
+user_in = data['exercises'][0]['name']
+dur_min = data['exercises'][0]['duration_min']
+nf_cal = data['exercises'][0]['nf_calories']
+
+
+url_sheety = 'YOUR_SHEETY_ENDPOINT'
+
+time_now = datetime.now()
+date_n = time_now.strftime("%d/%m/%Y")
+time_n = time_now.strftime('%H:%M:%S')
+
+
+body = {
+  'workout': {"date": date_n,
+        "time": time_n,
+        "exercise": user_in,
+        "duration": dur_min,
+        "calories": nf_cal,
+  }
+}
+
+response_sheety = requests.post(
+    url=url_sheety,
+    json=body,
+    auth=("id", "password")
+)
+print(response_sheety.status_code)
+print(response_sheety.text)
